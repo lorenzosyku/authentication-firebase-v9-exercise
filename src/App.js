aeee050c351b43d72eb600167b01093f0cfbb3b1
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-01-20 17:44:00 -0500

    added logout function & protection against spams costum hook to identify user

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import "./App.css";
-import { signUp } from "./firebase";
+import { signUp, useAuth, logout } from "./firebase";
 import { useRef, useState } from "react";
 
 function App() {
-
-  const emailRef = useRef()
-  const passwordRef = useRef()
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const [loading, setLoading] = useState(false);
+  const currentUser = useAuth();
 
   const handleSubmit = async () => {
-    await signUp(emailRef.current.value, passwordRef.current.value);
+    try {
+      setLoading(true);
+      await signUp(emailRef.current.value, passwordRef.current.value);
+    } catch (error) {
+      alert("error");
+    }
+
+    setLoading(false);
+    
+  };
+
+  const handleLogout = async () => {
+    try {
+      setLoading(true);
+      await logout();
+    } catch (error) {
+      alert("error");
+    }
+    setLoading(false);
   };
 
   return (
     <div className="">
       <div className="">
+        <h1>current user:{currentUser?.email}</h1>
+      </div>
+      <div className="">
         <input ref={emailRef} placeholder="email" />
         <input ref={passwordRef} type="passwordrun" placeholder="password" />
-        <button onClick={handleSubmit}>signup</button>
+        <button disabled={loading || currentUser} onClick={handleSubmit}>
+          signup
+        </button>
+        <button disabled={loading || !currentUser} onClick={handleLogout}>logout</button>
       </div>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
+import {getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signOut} from 'firebase/auth';
+import { useState, useEffect } from "react";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,6 +19,24 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 
+//signup
 export function signUp(email, password){
   return createUserWithEmailAndPassword(auth, email, password)
+}
+
+//logout
+export function logout(){
+  return signOut(auth);
+}
+
+//costum hook
+export function useAuth(){
+  const [currentUser, setCurrentUser] = useState();
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, user => setCurrentUser(user));
+    return unsub;
+  }, [])
+
+  return currentUser
 }
